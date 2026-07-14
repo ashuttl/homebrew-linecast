4a42008e314e005688b708d072d494447587aff2
Andrew Fowler <ashuttl@Andrews-MacBook-Pro.local>
2026-07-14 10:02:45 -0400

    Mention radar in the description

--- Formula/linecast.rb
+++ Formula/linecast.rb
@@ -1,7 +1,7 @@
 class Linecast < Formula
   include Language::Python::Virtualenv
 
-  desc "Terminal weather, solar arc, and tide visualizations"
+  desc "Terminal weather, radar, solar arc, and tide visualizations"
   homepage "https://github.com/ashuttl/linecast"
   url "https://files.pythonhosted.org/packages/4c/81/420c57131b4e8885fb062bbff9974f411b6862772c244f27fd8fe65736e3/linecast-1.3.0.tar.gz"
   sha256 "8545f6e08dfba8656c930deb49e1c5775f45fccd4306ca4e6c192b5aa7945d69"
