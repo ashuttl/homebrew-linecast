bae0242d082d1d679f8fc1fa056ad86eca923720
Andrew Shuttleworth Fowler <ashuttleworth@gmail.com>
2026-08-15 17:23:10 -0400

    linecast v1.9.0

--- Formula/linecast.rb
+++ Formula/linecast.rb
@@ -3,8 +3,8 @@ class Linecast < Formula
 
   desc "Terminal weather, radar, solar arc, and tide visualizations"
   homepage "https://github.com/ashuttl/linecast"
-  url "https://files.pythonhosted.org/packages/a2/9b/2e22a590b91ffbe22f7e0af75b1ac1472c4cd269ca835368285e8c8df30d/linecast-1.8.0.tar.gz"
-  sha256 "a963f7cad42eb27c094dfc233ef2b511d93c14c9283ff7e283a34ad1162684e3"
+  url "https://files.pythonhosted.org/packages/75/f3/393f9520901e52d1e14a1a6dc0bd967f98de320586a5768c5c19f306f0bd/linecast-1.9.0.tar.gz"
+  sha256 "be6943de48460576e2cd39229c29b26108ec01a64464065c1b417e7832dc5df7"
   license "MIT"
 
   depends_on "python@3.13"
